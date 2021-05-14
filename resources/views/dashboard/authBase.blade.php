3de4771ea8cd197c4d2e9affccbc27a12ce51956
Hansenhalim <fpsecond.hh@gmail.com>
2021-05-14 18:39:31 +0700

    Fixed gtag

--- resources/views/dashboard/authBase.blade.php
+++ resources/views/dashboard/authBase.blade.php
@@ -36,20 +36,19 @@
     <meta name="theme-color" content="#ffffff">
     <!-- Icons-->
     <link href="{{ asset('css/free.min.css') }}" rel="stylesheet"> <!-- icons -->
-    <link href="{{ asset('css/flag-icon.min.css') }}" rel="stylesheet"> <!-- icons -->
+    <link href="{{ asset('css/brand.min.css') }}" rel="stylesheet"> <!-- icons -->
+    <link href="{{ asset('css/flag.min.css') }}" rel="stylesheet"> <!-- icons -->
     <!-- Main styles for this application-->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-    <!-- Global site tag (gtag.js) - Google Analytics-->
-    <script async="" src="https://www.googletagmanager.com/gtag/js?id=G-QEKCMQQZ16"></script>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-FY0HC3B1E6"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
-
-      function gtag() {
-        dataLayer.push(arguments);
-      }
+      function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-      // Shared ID
-      gtag('config', 'G-QEKCMQQZ16');
+
+      gtag('config', 'G-FY0HC3B1E6');
     </script>
 
     <link href="{{ asset('css/coreui-chartjs.css') }}" rel="stylesheet">
