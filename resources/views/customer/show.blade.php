c77144013d4c31a82b2777f6207fa68395f19cd5
Hansenhalim <fpsecond.hh@gmail.com>
2021-05-30 16:09:07 +0700

    working

--- resources/views/customer/show.blade.php
+++ resources/views/customer/show.blade.php
@@ -60,7 +60,7 @@
                   </thead>
                   <tbody>
                     <tr class="table-light">
-                      <th class="text-right"><input type="checkbox" class="unt-chk"></th>
+                      <th class="text-right"><input type="checkbox" class="unt-chck"></th>
                       <th class="text-center">{{ $loop->iteration }}</th>
                       <th><a class="text-dark" href="{{ route('units.show', ['unit' => $unit->id]) }}">{{ $unit->name }}</a></th>
                       <th><a class="text-dark" href="{{ route('customers.show', ['customer' => $unit->customer->id]) }}">{{ $unit->customer->name }}</a></th>
@@ -80,27 +80,25 @@
                     </thead>
                     <tbody>
                       @forelse ($unit->months as $month)
-                        <tr>
+                        <tr class="mth">
                           <th colspan="4"></th>
-                          <th class="text-right"><input type="checkbox" class="mth-chk"></th>
+                          <th class="text-right"><input type="checkbox" class="mth-chck"></th>
                           <th class="text-center">{{ $loop->iteration }}</th>
                           <td>{{ $month['period']->format('M Y') }}</td>
                           <td class="text-right">{{ number_format($month['credit']) }}</td>
                           <td class="text-right">{{ number_format($month['fine']) }}</td>
                         </tr>
-                        <tbody>
-                          <tr class="table-secondary">
-                            <th colspan="8" class="text-right">Tagihan</th>
-                            <th class="text-right">{{ number_format($month['credit'] + $month['fine']) }}</th>
-                          </tr>
-                          <tr class="table-secondary">
-                            <th colspan="10" class="text-right"><button class="btn btn-square btn-outline-success"><i class="cil-wallet"></i>&nbsp;Add Payments</button></th>
-                          </tr>
-                          <tr class="table-secondary">
-                            <th colspan="8" class="text-right">Sisa</th>
-                            <th class="text-right text-danger">{{ number_format($month['credit'] + $month['fine']) }}</th>
-                          </tr>
-                        </tbody>
+                        <tr class="pymnt-sctn table-secondary">
+                          <th colspan="8" class="text-right">Tagihan</th>
+                          <th class="text-right">{{ number_format($month['credit'] + $month['fine']) }}</th>
+                        </tr>
+                        <tr class="pymnt-sctn table-secondary table-borderless">
+                          <th colspan="10" class="text-right"><button class="btn btn-square btn-outline-success"><i class="cil-wallet"></i>&nbsp;Add Payments</button></th>
+                        </tr>
+                        <tr class="pymnt-sctn table-secondary table-borderless">
+                          <th colspan="8" class="text-right">Sisa</th>
+                          <th class="text-right text-danger">{{ number_format($month['credit'] + $month['fine']) }}</th>
+                        </tr>
                       @empty
                         <tr class="table-success">
                           <td colspan="10" style="text-align: center">No tunggak-tunggak club :)</td>
@@ -116,7 +114,7 @@
             </div>
             <div class="card-footer d-flex justify-content-between">
               <button id="dbg-btn" type="button"></i>debug-btn</button>
-              <button id="sbt-btn" type="submit" class="btn btn-primary btn-secondary" disabled></i>Submit</button>
+              <button id="sbmt-btn" type="submit" class="btn btn-primary btn-secondary" disabled></i>Submit</button>
             </div>
           </div>
         </div>
@@ -124,10 +122,11 @@
     </div>
   </div>
   <script>
-    submitBtn = document.getElementById('sbt-btn')
+    submitBtn = document.getElementById('sbmt-btn')
     debugBtn = document.getElementById('dbg-btn')
-    unitChecks = document.getElementsByClassName('unt-chk')
-    monthChecks = document.getElementsByClassName('mth-chk')
+    unitChecks = document.getElementsByClassName('unt-chck')
+    monthChecks = document.getElementsByClassName('mth-chck')
+    paymentSection = document.getElementsByClassName('pymnt-sctn')
 
     function validateSubmission() {
       submitBtn.disabled = !submitBtn.disabled
@@ -138,9 +137,9 @@
       console.log('echo function executed :)')
     }
 
-    // function togglePayment(params) {
-      
-    // }
+    function togglePayment(month) {      
+      console.log(month)
+    }
     
     debugBtn.addEventListener('click', validateSubmission)
 
@@ -150,12 +149,16 @@
         unit              = unitCheckWrapper.parentNode.parentNode
         monthsHeader      = unit.nextElementSibling
         months            = monthsHeader.nextElementSibling
-        unitMonthChecks   = months.getElementsByClassName('mth-chk')
+        unitMonthChecks   = months.getElementsByClassName('mth-chck')
 
         if (this.checked) for (unitMonthCheck of unitMonthChecks) unitMonthCheck.checked = true
         else for (unitMonthCheck of unitMonthChecks) unitMonthCheck.checked = false
 
-        echo()
+        months = months.getElementsByClassName('mth')
+        for (month of months) {
+          togglePayment(month)
+        }
+        
       })
     }
 
@@ -164,16 +167,16 @@
         monthCheckWrapper = this.parentNode
         month             = monthCheckWrapper.parentNode
         months            = month.parentNode
-        unitMonthChecks   = months.getElementsByClassName('mth-chk')
+        unitMonthChecks   = months.getElementsByClassName('mth-chck')
         monthsHeader      = months.previousElementSibling
         monthUnit         = monthsHeader.previousElementSibling
-        monthUnitCheck    = monthUnit.getElementsByClassName('unt-chk')[0]
+        monthUnitCheck    = monthUnit.getElementsByClassName('unt-chck')[0]
 
         checked = true
         for (unitMonthCheck of unitMonthChecks) if (checked) checked = unitMonthCheck.checked
         monthUnitCheck.checked = checked
 
-        echo()
+        togglePayment(month)
       })
     }
     
