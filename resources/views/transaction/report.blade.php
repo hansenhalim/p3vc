b115268b7ebe1ed619ff51c5ae47323d2a0aef19
Hansenhalim <fpsecond.hh@gmail.com>
2021-06-22 14:31:24 +0700

    limit 7 days

--- resources/views/transaction/report.blade.php
+++ resources/views/transaction/report.blade.php
@@ -131,9 +131,12 @@
           'Today': [moment(), moment()],
           'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
           'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-          'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-          'This Month': [moment().startOf('month'), moment().endOf('month')],
-          'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+          // 'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+          // 'This Month': [moment().startOf('month'), moment().endOf('month')],
+          // 'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        maxSpan: {
+          days: 7
         },
       }, cb)
 
