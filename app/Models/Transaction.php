1d6e6e4515cd5af1f336679a833f63c3035a68e5
Hansenhalim <fpsecond.hh@gmail.com>
2021-06-29 00:09:19 +0700

    Implement ApprovedScope to transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\ApprovedScope;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
@@ -21,6 +22,11 @@ class Transaction extends Model
   public function payments()
   {
     return $this->belongsToMany(Payment::class)->withPivot('amount');
+  }
+
+  protected static function booted()
+  {
+    static::addGlobalScope(new ApprovedScope);
   }
 
   public function getPeriodAttribute($value)
