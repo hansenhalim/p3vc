a5be33b5dc9334319067b3998d52b6667a9aa289
Hansenhalim <fpsecond.hh@gmail.com>
2021-07-24 06:55:22 +0700

    Adjustment in header and footer

--- resources/views/dashboard/shared/footer.blade.php
+++ resources/views/dashboard/shared/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="c-footer">
-  <div><a href="https://hann.my.id">Hansen Halim</a> &copy; 2021 P3VC.</div>
-  <div class="ml-auto">Powered by&nbsp;<a href="https://coreui.io/">CoreUI</a></div>
+  <div><a href="https://hann.my.id" target="_blank" rel="noopener noreferrer">Hansen Halim</a> &copy; 2021 P3VC.</div>
+  <div class="ml-auto">Powered by&nbsp;<a href="https://coreui.io/" target="_blank" rel="noopener noreferrer">CoreUI</a></div>
 </footer>
