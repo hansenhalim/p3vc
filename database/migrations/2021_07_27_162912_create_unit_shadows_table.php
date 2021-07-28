967ea925e570993e924197640b6684482b33abff
Hansenhalim <fpsecond.hh@gmail.com>
2021-07-28 09:08:54 +0700

    Create configs and unit_shadows migration Create ConfigSeeder

---
+++ database/migrations/2021_07_27_162912_create_unit_shadows_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnitShadowsTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('unit_shadows', function (Blueprint $table) {
+      $table->unsignedBigInteger('customer_id');
+      $table->string('name'); 
+      $table->string('customer_name');
+      $table->string('cluster_name');
+      $table->float('area_sqm', 6, 2);
+      $table->integer('balance');
+      $table->integer('debt');
+      $table->unsignedInteger('months_count');
+      $table->unsignedInteger('months_total');
+      $table->unsignedInteger('credit');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('unit_shadows');
+  }
+}
