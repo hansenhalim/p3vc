07a6900682f650a5c484a354c2b230011dd8e4c8
Hansenhalim <fpsecond.hh@gmail.com>
2021-08-04 18:14:53 +0700

    hot fix

--- app/Exports/UnitsLinkajaExport.php
+++ app/Exports/UnitsLinkajaExport.php
@@ -12,7 +12,7 @@ class UnitsLinkajaExport implements FromQuery, WithHeadings, WithStrictNullCompa
   public function query()
   {
     return DB::table('unit_shadows')
-      ->select(DB::raw('idlink,customer_name,DATE_FORMAT(DATE_SUB((SELECT value from configs where `key`="units_last_sync"),INTERVAL 1 MONTH),"%Y%m"),DATE_FORMAT(LAST_DAY((SELECT value from configs where `key`="units_last_sync")),"%Y%m%d"),credit,2500,credit+2500'))
+      ->select(DB::raw('idlink,customer_name,DATE_FORMAT(DATE_SUB((SELECT value from configs where `key`="units_last_sync"),INTERVAL 1 MONTH),"%Y%m"),DATE_FORMAT(LAST_DAY((SELECT value from configs where `key`="units_last_sync")),"%Y%m%d"),months_total,2500,credit+2500'))
       ->orderBy('id');
   }
 
