775d67fe0415ac7f0a925fc5a7279962b4e14e0b
Hansen Halim <fpsecond.hh@gmail.com>
2022-07-04 00:12:38 +0700

    wip

--- config/database.php
+++ config/database.php
@@ -61,6 +61,8 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 PDO::MYSQL_ATTR_LOCAL_INFILE => true,
+                PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+                PDO::ATTR_EMULATE_PREPARES => true,
             ]) : [],
         ],
 
